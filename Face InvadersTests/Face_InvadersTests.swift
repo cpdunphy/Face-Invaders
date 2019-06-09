282f0b366fa4ff929126a56f28ca2b7218773dd7
Collin Dunphy <cpdun03@gmail.com>
2019-06-09 11:15:40 -0400

    Initial Commit

---
+++ Face InvadersTests/Face_InvadersTests.swift
@@ -0,0 +1,34 @@
+//
+//  Face_InvadersTests.swift
+//  Face InvadersTests
+//
+//  Created by Collin on 6/9/19.
+//  Copyright © 2019 Collin Dunphy Apps. All rights reserved.
+//
+
+import XCTest
+@testable import Face_Invaders
+
+class Face_InvadersTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
