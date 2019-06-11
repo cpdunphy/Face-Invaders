9856f112a255656205a3790111d34f0e647ec6c4
Collin Dunphy <cpdun03@gmail.com>
2019-06-10 21:33:15 -0400

    Finish Contact method, explosion effects (fading + soundeffects), added the base for the score keeper (needs fine tuning), started custom fonts

--- Face Invaders/GameScene.swift
+++ Face Invaders/GameScene.swift
@@ -9,11 +9,23 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
-    
-    let player = SKSpriteNode(imageNamed: "player")
-    
+class GameScene: SKScene, SKPhysicsContactDelegate {
+
+    //Declaring outside allows for global access
+    //Wrench Sound Setup
     let wrenchSound = SKAction.playSoundFileNamed("wrenchSoundEffect.m4a", waitForCompletion: false)
+    let explosionSound = SKAction.playSoundFileNamed("explosionSoundEffect.m4a", waitForCompletion: false)
+    let player = SKSpriteNode(imageNamed: "player")
+
+    var gameScore = 0
+    let scoreLabel = SKLabelNode(fontNamed: "Aquatico")
+    
+    struct PhysicsCategories {
+        static let None: UInt32 = 0
+        static let Player: UInt32 = 0b1 //1 (binary)
+        static let Wrench: UInt32 = 0b10 //2 (binary)
+        static let Enemy: UInt32 = 0b100 //4 (binary)
+    }
     
     let gameArea: CGRect
     
@@ -21,9 +33,10 @@ class GameScene: SKScene {
         return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
     }
     
-    func random(min min: CGFloat, max: CGFloat) -> CGFloat {
+    func random(min: CGFloat, max: CGFloat) -> CGFloat {
         return random() * (max - min) + min
     }
+
     
     override init(size: CGSize) {
         
@@ -34,12 +47,14 @@ class GameScene: SKScene {
         
         super.init(size: size)
     }
-    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    
+    
     override func didMove(to view: SKView) {
+        self.physicsWorld.contactDelegate = self
         
         let background = SKSpriteNode(imageNamed: "background_blue")
         background.size = self.size
@@ -50,16 +65,110 @@ class GameScene: SKScene {
         player.setScale(0.4)
         player.position = CGPoint(x: self.size.width/2, y: self.size.height * 0.2)
         player.zPosition = 2
+        player.physicsBody = SKPhysicsBody(rectangleOf: player.size)
+        player.physicsBody!.affectedByGravity = false
+        player.physicsBody!.categoryBitMask = PhysicsCategories.Player
+        player.physicsBody!.collisionBitMask = PhysicsCategories.None
+        player.physicsBody!.contactTestBitMask = PhysicsCategories.Enemy
         self.addChild(player)
+        
+        scoreLabel.text = "Score: 0"
+        scoreLabel.fontSize = 70
+        scoreLabel.fontColor = SKColor.white
+        scoreLabel.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.left
+        scoreLabel.position = CGPoint(x: self.size.width * 0.15, y: self.size.height * 0.9)
+        scoreLabel.zPosition = 100
+        self.addChild(scoreLabel)
+        
+        
+        startNewLevel()
     }
     
+    func addScore() {
+        gameScore += 1
+        scoreLabel.text = "Score: \(gameScore)"
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        var body1 = SKPhysicsBody()
+        var body2 = SKPhysicsBody()
+        
+        if contact.bodyA.categoryBitMask.byteSwapped < contact.bodyB.categoryBitMask.byteSwapped {
+            body1 = contact.bodyA
+            body2 = contact.bodyB
+        }
+        else {
+            body1 = contact.bodyB
+            body2 = contact.bodyA
+        }
+        
 
+        
+
+        if body1.categoryBitMask == PhysicsCategories.Player && body2.categoryBitMask == PhysicsCategories.Enemy {
+            //if the player hits the enemy
+            if body1.node != nil {
+                spawnExplosion(spawnPosition: body1.node!.position)
+            }
+            if body2.node != nil {
+                spawnExplosion(spawnPosition: body2.node!.position)
+            }
+            body1.node?.removeFromParent()
+            body2.node?.removeFromParent()
+        }
+        
+        if body1.categoryBitMask == PhysicsCategories.Wrench && body2.categoryBitMask == PhysicsCategories.Enemy {
+            
+            addScore()
+            
+            if body2.node != nil {
+                if body2.node!.position.y > self.size.height {
+                    return
+                }
+                else {
+                    spawnExplosion(spawnPosition: body2.node!.position)
+                }
+            }
+            body1.node?.removeFromParent()
+            body2.node?.removeFromParent()
+        }
+    }
+    
+    func spawnExplosion(spawnPosition: CGPoint) {
+        let explosion = SKSpriteNode(imageNamed: "explosion")
+        explosion.setScale(0.05)
+        explosion.position = spawnPosition
+        explosion.zPosition = 3
+        self.addChild(explosion)
+        
+        let scaleIn = SKAction.scale(to: 1, duration: 0.1)
+        let fadeOut = SKAction.fadeOut(withDuration: 0.1)
+        let delete = SKAction.removeFromParent()
+        let explosionSequence = SKAction.sequence([explosionSound,scaleIn,fadeOut,delete])
+        
+        explosion.run(explosionSequence)
+    }
+    
+    
+    
+    func startNewLevel() {
+        let spawn = SKAction.run(spawnEnemy)
+        let waitToSPawn = SKAction.wait(forDuration: 1.5)
+        let spawnSequence = SKAction.sequence([spawn,waitToSPawn])
+        let spawnForever = SKAction.repeatForever(spawnSequence)
+        self.run(spawnForever)
+    }
     
     func throwWrench() {
         let wrench = SKSpriteNode(imageNamed: "wrench")
         wrench.setScale(0.15)
         wrench.position = player.position
         wrench.zPosition = 1
+        wrench.physicsBody = SKPhysicsBody(rectangleOf: wrench.size)
+        wrench.physicsBody!.affectedByGravity = false
+        wrench.physicsBody!.categoryBitMask = PhysicsCategories.Wrench
+        wrench.physicsBody!.collisionBitMask = PhysicsCategories.None
+        wrench.physicsBody!.contactTestBitMask = PhysicsCategories.Enemy
         self.addChild(wrench)
     
         let moveWrench = SKAction.moveTo(y: self.size.height + wrench.size.height, duration: 1)
@@ -80,6 +189,11 @@ class GameScene: SKScene {
         enemy.setScale(0.75)
         enemy.position = startPoint
         enemy.zPosition = 2
+        enemy.physicsBody = SKPhysicsBody(rectangleOf: enemy.size)
+        enemy.physicsBody!.affectedByGravity = false
+        enemy.physicsBody!.categoryBitMask = PhysicsCategories.Enemy
+        enemy.physicsBody!.collisionBitMask = PhysicsCategories.None
+        enemy.physicsBody!.contactTestBitMask = PhysicsCategories.Player | PhysicsCategories.Wrench
         self.addChild(enemy)
     
         let moveEnemy = SKAction.move(to: endPoint, duration: 1.5)
@@ -96,7 +210,6 @@ class GameScene: SKScene {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         throwWrench()
-        spawnEnemy()
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -108,14 +221,12 @@ class GameScene: SKScene {
             
             player.position.x += amountDragged
             
-            if player.position.x <= gameArea.maxX - player.size.width, player.position.x >= gameArea.minX + player.size.width {
-                player.position.x += amountDragged
-            }
-            else if player.position.x < gameArea.minX + player.size.width {
-                player.position.x = gameArea.minX + player.size.width
-            }
-            else if player.position.x > gameArea.maxX - player.size.width {
+            if player.position.x > gameArea.maxX - player.size.width {
                 player.position.x = gameArea.maxX - player.size.width
+            }
+            
+            if player.position.x < gameArea.minX + player.size.width {
+                player.position.x = gameArea.minX + player.size.width
             }
             
         }
